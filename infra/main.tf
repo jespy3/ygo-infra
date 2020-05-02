a94dc33d470fbfa59f8895f00587ea30ecfcf2f2
Josh <josh.espinas@xero.com>
2020-05-02 14:29:17 +1200

    Creates first security group

---
+++ infra/main.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "allow_tls" {
+  name = "allow_tls"
+  description = "Allows inbound TLS traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port =  443
+    to_port = 443
+    protocol = "tcp"
+  }
+
+  egress {
+    description = "Allows a sending information to anywhere."
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  }
+
+  tags = {
+    project = "ygo"
+  }
+}
